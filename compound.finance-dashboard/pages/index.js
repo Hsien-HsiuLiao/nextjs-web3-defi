531c6cdc0c0e642674fc27e1aaaa57d93795d742
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-06-25 16:28:52 +0000

    add loading message

--- compound.finance-dashboard/pages/index.js
+++ compound.finance-dashboard/pages/index.js
@@ -3,11 +3,26 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Compound from '@compound-finance/compound-js';
 import calculateApy from '../apy.js';
+import { useEffect, useState } from 'react';
+
 
 
 export default function Home({ apys }) {
+  const [loading, setLoading] = useState(true);
+
   const formatPercent = number =>
     `${new Number(number).toFixed(2)}%`
+
+  useEffect(() => {
+    // Simulate data fetching
+    if (apys.length === 0) {
+      setLoading(true);
+      // Fetch data here if needed
+      setLoading(false);
+    } else {
+      setLoading(false);
+    }
+  }, [apys]);
 
   return (
     <div className='container'>
@@ -20,49 +35,46 @@ export default function Home({ apys }) {
         <div className='col-sm-12'>
           <div className="jumbotron">
             <h1 className='text-center'>Compound Dashboard</h1>
-            <h5 className="display-4 text-center">Shows Compound APYs <br /> with COMP token rewards</h5>
+            <h5 className="display-4 text-center"> Compound APYs <br /> with COMP token rewards</h5>
           </div>
         </div>
       </div>
 
 
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Ticker</th>
-            <th>Supply APY</th>
-            <th>COMP APY</th>
-            <th>Total APY</th>
-          </tr>
-        </thead>
-        <tbody>
-          {apys && apys.map(apy => (
-            <tr key={apy.ticker}>
-              <td>
-                {/*  <img */}
-                {/*    style=width: 25, height: 25, marginRight: 10 */}
-                <Image
-                  src={`/img/${apy.ticker.toLowerCase()}.png`}
-                  width={25}
-                  height={25}
-                  //        marginRight={10}
-                  alt='image'
-                />
-                {apy.ticker.toUpperCase()}
-              </td>
-              <td>
-                {formatPercent(apy.supplyApy)}
-              </td>
-              <td>
-                {formatPercent(apy.compApy)}
-              </td>
-              <td>
-                {formatPercent(parseFloat(apy.supplyApy) + parseFloat(apy.compApy))}
-              </td>
+      {loading ? (
+        <div className="text-center">
+          <h2>Loading Data...</h2>
+        </div>
+      ) : (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Ticker</th>
+              <th>Supply APY</th>
+              <th>COMP APY</th>
+              <th>Total APY</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {apys.map(apy => (
+              <tr key={apy.ticker}>
+                <td>
+                  <Image
+                    src={`/img/${apy.ticker.toLowerCase()}.png`}
+                    width={25}
+                    height={25}
+                    alt='image'
+                  />
+                  {apy.ticker.toUpperCase()}
+                </td>
+                <td>{formatPercent(apy.supplyApy)}</td>
+                <td>{formatPercent(apy.compApy)}</td>
+                <td>{formatPercent(parseFloat(apy.supplyApy) + parseFloat(apy.compApy))}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   )
 }
@@ -74,7 +86,7 @@ export async function getServerSideProps(context) {
     calculateApy(Compound.cDAI, 'DAI'),
     calculateApy(Compound.cUSDC, 'USDC'),
     calculateApy(Compound.cUSDT, 'USDT'),
-  //  calculateApy(Compound.cBAT, 'BAT'),
+    //  calculateApy(Compound.cBAT, 'BAT'),
     //calculateApy(Compound.cUNI, 'UNI')
 
     // add other tokens 
